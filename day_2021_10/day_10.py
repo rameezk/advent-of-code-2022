94f1f7d6a66c33429c29a2f8c9edf4dba341d80b
Rameez Khan <rameezkhan.sa@gmail.com>
2022-11-24 11:04:07 +0200

    Add 2021 day 10 part 2

--- day_2021_10/day_10.py
+++ day_2021_10/day_10.py
@@ -1,6 +1,8 @@
-from common.timing import timeit
 import typing
 from enum import StrEnum, auto
+from functools import reduce
+
+from common.timing import timeit
 
 
 class Status(StrEnum):
@@ -12,8 +14,12 @@ class Status(StrEnum):
 VALID_CHUNKS = [("(", ")"), ("[", "]"), ("{", "}"), ("<", ">")]
 OPENING_BRACES = [b[0] for b in VALID_CHUNKS]
 CLOSING_BRACES = [b[1] for b in VALID_CHUNKS]
-PAIR_LOOKUP = {c: o for o, c in zip(OPENING_BRACES, CLOSING_BRACES)}
-SCORE_LOOKUP = {")": 3, "]": 57, "}": 1197, ">": 25137}
+PAIR_LOOKUP = {
+    **{c: o for o, c in zip(OPENING_BRACES, CLOSING_BRACES)},
+    **{o: c for o, c in zip(OPENING_BRACES, CLOSING_BRACES)},
+}
+CORRUPTED_CHAR_SCORE_LOOKUP = {")": 3, "]": 57, "}": 1197, ">": 25137}
+CORRECT_CHAR_SCORE_LOOKUP = {")": 1, "]": 2, "}": 3, ">": 4}
 
 
 def check(chunks: typing.List[str]) -> (Status, typing.Optional[str]):
@@ -32,6 +38,25 @@ def check(chunks: typing.List[str]) -> (Status, typing.Optional[str]):
         return Status.INCOMPLETE, None
 
 
+def check_and_correct(
+    chunks: typing.List[str],
+) -> (Status, typing.Optional[typing.List[str]]):
+    """Check for valid chunks and suggests the correct completion."""
+    s = []
+    for c in chunks:
+        if c in OPENING_BRACES:
+            s.append(c)
+        else:
+            b = s.pop()
+            if PAIR_LOOKUP[c] != b:
+                return Status.CORRUPTED, None
+    if len(s) == 0:
+        return Status.VALID, None
+    else:
+        correct_characters = [PAIR_LOOKUP[c] for c in s[::-1]]
+        return Status.INCOMPLETE, correct_characters
+
+
 @timeit
 def p1() -> int:
     illegal_characters = []
@@ -41,20 +66,36 @@ def p1() -> int:
             status, first_illegal_char = check(chunks)
             if status == Status.CORRUPTED:
                 illegal_characters.append(first_illegal_char)
-    score = sum([SCORE_LOOKUP[c] for c in illegal_characters])
+    score = sum([CORRUPTED_CHAR_SCORE_LOOKUP[c] for c in illegal_characters])
     return score
 
 
 @timeit
-def p2():
-    ...
+def p2() -> int:
+    scores = []
+    with open("./input.txt", "r") as file:
+        for line in file:
+            chunks = list(line.strip())
+            status, correct_characters = check_and_correct(chunks)
+            if status == Status.INCOMPLETE:
+                score = reduce(
+                    lambda s, c: (s * 5) + CORRECT_CHAR_SCORE_LOOKUP[c],
+                    correct_characters,
+                    0,
+                )
+                scores.append(score)
+    middle_score = sorted(scores)[(len(scores) - 1) // 2]
+    return middle_score
 
 
 if __name__ == "__main__":
     print("======p1=====")
     p1 = p1()
+    assert p1 == 193275
     print(p1)
     print("======p1=====")
     print("======p2=====")
-    # p2()
+    p2 = p2()
+    assert p2 == 2429644557
+    print(p2)
     print("======p2=====")
