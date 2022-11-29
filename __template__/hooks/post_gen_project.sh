8580a0dbdc658a0ff093d36fe1453779349aa2e0
Rameez Khan <rameezkhan.sa@gmail.com>
2022-11-29 08:27:02 +0200

    Prepare for 2022

--- __template__/hooks/post_gen_project.sh
+++ __template__/hooks/post_gen_project.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Downloading puzzle input for day {{cookiecutter.aoc_day}}"
-url="https://adventofcode.com/2021/day/{{cookiecutter.aoc_day}}/input"
+url="https://adventofcode.com/2022/day/{{cookiecutter.aoc_day}}/input"
 session=$(cat ../.session)
 curl $url -H "Cookie: session=$session" --output input.txt --silent
