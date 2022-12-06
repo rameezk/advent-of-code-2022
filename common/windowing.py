e4a7545f88434f9be3a57dc5a803cd83abd98946
Rameez Khan <rameezkhan.sa@gmail.com>
2022-12-06 10:50:43 +0200

    Add sliding window helper

--- common/windowing.py
+++ common/windowing.py
@@ -3,3 +3,10 @@ import typing
 
 def window(lst: typing.List, size: int = 2) -> typing.List:
     return [lst[i : i + size] for i in range(len(lst))]
+
+
+def sliding_window(
+    seq: typing.Sequence, size: int
+) -> typing.Generator[typing.Sequence, None, None]:
+    for i in range(len(seq) - size + 1):
+        yield seq[i : i + size]
--- day_06/day_06_p1.py
+++ day_06/day_06_p1.py
@@ -1,3 +1,6 @@
+from common.windowing import sliding_window
+
+
 def is_unique(s):
     d = []
     for c in s:
@@ -15,10 +18,9 @@ def solve_p1():
     with open("./input.txt") as file:
         line = file.read()
 
-    for char_count, i in enumerate(range(len(line) - WIN_SIZE + 1)):
-        thing = line[i : i + WIN_SIZE]
-        if is_unique(thing):
-            return char_count + WIN_SIZE
+    for char_idx, window in enumerate(sliding_window(line, WIN_SIZE)):
+        if is_unique(window):
+            return char_idx + WIN_SIZE
 
 
 if __name__ == "__main__":
