8e2d9c508546b91a7bbf46017fce250351ebf737
Rameez Khan <rameezkhan.sa@gmail.com>
2022-12-14 11:57:46 +0200

    Add day 14

---
+++ day_14/day_14_p2.py
@@ -0,0 +1,125 @@
+from collections import defaultdict
+from enum import StrEnum, auto
+from itertools import pairwise
+
+
+class CaveItem(StrEnum):
+    ROCK = auto()
+    AIR = auto()
+    SAND = auto()
+    HOLE = auto()
+
+
+CAVE = dict[tuple[int, int], CaveItem]
+HOLE = (500, 0)
+
+
+def parse_cave(filename: str) -> CAVE:
+    cave = defaultdict(lambda: CaveItem.AIR)
+
+    with open(filename) as file:
+        for line in file:
+            path = list(
+                map(
+                    lambda p: list(map(int, p.strip().split(","))),
+                    line.strip().split("->"),
+                )
+            )
+            path_pairs = pairwise(path)
+            for (start_x, start_y), (end_x, end_y) in path_pairs:
+                if start_x == end_x:
+                    step = 1 if end_y > start_y else -1
+                    for y in range(start_y, end_y + step, step):
+                        cave[start_x, y] = CaveItem.ROCK
+                elif start_y == end_y:
+                    step = 1 if end_x > start_x else -1
+                    for x in range(start_x, end_x + step, step):
+                        cave[x, start_y] = CaveItem.ROCK
+                else:
+                    raise AssertionError(f"Cannot handle this path")
+
+    cave[HOLE] = CaveItem.HOLE
+    return cave
+
+
+def print_cave(cave: CAVE, min_x: int, max_x: int, min_y: int, max_y: int) -> None:
+    print()
+    for y in range(min_y, max_y + 1):
+        for x in range(min_x, max_x + 1):
+            item = cave[x, y]
+            match item:
+                case CaveItem.ROCK:
+                    print("#", end="")
+                case CaveItem.AIR:
+                    print(".", end="")
+                case CaveItem.SAND:
+                    print("O", end="")
+                case CaveItem.HOLE:
+                    print("+", end="")
+                case _:
+                    ...
+        print()
+
+
+def cave_bounds(cave: CAVE) -> tuple[int, int, int, int]:
+    keys = cave.keys()
+    x_s = [x for x, _ in keys]
+    min_x, max_x = min(x_s), max(x_s)
+    y_s = [y for _, y in keys]
+    min_y, max_y = min(y_s), max(y_s)
+    return min_x, max_x, min_y, max_y
+
+
+def is_blocked(cave: CAVE, x, y) -> bool:
+    return cave[x, y] in [CaveItem.ROCK, CaveItem.SAND]
+
+
+def drop_sand(cave: CAVE, floor: int) -> (CAVE, bool):
+    x, y = HOLE
+
+    # Check if hole is blocked
+    if cave[x, y] == CaveItem.SAND:
+        return cave, False
+
+    while True:
+        y += 1
+
+        # hit floor
+        if y == floor:
+            cave[x, y - 1] = CaveItem.SAND
+            break
+
+        if not is_blocked(cave, x, y):
+            continue
+        else:
+            if not is_blocked(cave, x - 1, y):
+                x -= 1
+                continue
+
+            if not is_blocked(cave, x + 1, y):
+                x += 1
+                continue
+
+            cave[x, y - 1] = CaveItem.SAND
+            break
+
+    return cave, True
+
+
+def solve_p2(filename: str):
+    cave = parse_cave(filename)
+    _, _, _, max_y = cave_bounds(cave)
+
+    floor = 2 + max_y
+    can_continue = True
+    sand_dropped = -1
+    while can_continue:
+        sand_dropped += 1
+        cave, can_continue = drop_sand(cave, floor)
+    return sand_dropped
+
+
+if __name__ == "__main__":
+    p2 = solve_p2("./input.txt")
+    print(p2)
+    assert p2 == 26625
